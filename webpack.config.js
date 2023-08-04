438ffb9c4e22e91ad7783c13f3cc87a2c43cbb2c
O.Remilekun <remmylknolaotan@gmail.com>
2023-08-04 11:27:32 +0100

    Clean up for next lessons

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,4 @@
 const path = require('path');
-const yaml = require('yamljs');
-const json5 = require('json5');
-const toml = require('toml');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -29,49 +26,4 @@ module.exports = {
   optimization: {
     runtimeChunk: 'single',
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
-      {
-        test: /\.toml$/i,
-        type: 'json',
-        parser: {
-          parse: toml.parse,
-        },
-      },
-      {
-        test: /\.yaml$/i,
-        type: 'json',
-        parser: {
-          parse: yaml.parse,
-        },
-      },
-      {
-        test: /\.json5$/i,
-        type: 'json',
-        parser: {
-          parse: json5.parse,
-        },
-      },
-    ],
-  }
 };
