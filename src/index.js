510e52f18e5d03bcaa639e1fe9c2e6378e03a9a6
O.Remilekun <remmylknolaotan@gmail.com>
2023-08-04 10:54:10 +0100

    Load image using webpack

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import _ from "lodash";
 import printMe from "./print";
 import './style.css';
+import image from "./image.jpg";
 
 function component() {
   const element = document.createElement("div");
   const btn = document.createElement('button');
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add('hello');
+
+  // Add image to our existing div
+  const myImage = new Image();
+  myImage.src = image;
 
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
 
   element.appendChild(btn);
+  element.appendChild(myImage);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,12 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader']
-      }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
+    
   }
 };
