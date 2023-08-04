820841284917a753ece6f815c712ffddca5e83ec
O.Remilekun <remmylknolaotan@gmail.com>
2023-08-04 10:23:11 +0100

    Resumption to odin course

--- webpack.config.js
+++ webpack.config.js
@@ -25,4 +25,12 @@ module.exports = {
   optimization: {
     runtimeChunk: 'single',
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ],
+  }
 };
